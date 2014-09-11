d12a41d6fd4389ada553e51cecf0268f717a4e70
Gabriel Kabbe <gabriel.kabbe@mail.de>
2014-09-11 23:21:11 +0000

    image capturing and comparison between consecutive images with numpy

--- motion.py
+++ motion.py
@@ -2,31 +2,27 @@
 #TODO: opencv benutzen! -> img direkt nach numpy array?
 
 import cStringIO as StringIO
-import Image 
+#import Image 
+import cv2
 import picamera
 import numpy as np
-import time
-import pdb
+#import time
+#import ipdb
 
-camera = picamera.PiCamera()
-image_data = StringIO.StringIO()
-camera.capture(image_data, format="bmp", use_video_port=True)
-print len(image_data.getvalue())
-image_data.reset()
-im = Image.open(image_data)
-image_data.reset()
-#image_data.truncate()
-imarr1 = np.array(im)
-pdb.set_trace()
-
-while 1:
+with picamera.PiCamera() as camera:
 	image_data = StringIO.StringIO()
+	camera.resolution = (160, 120)
 	camera.capture(image_data, format="bmp", use_video_port=True)
-	image_data.seek(0)
-	im = Image.open(image_data)
-	imarr2 = np.array(im)
-#	pdb.set_trace()
-	print float(((imarr1-imarr2).sum(axis=2)**2).sum())/(imarr1.sum(axis=2)**2).sum()
-	imarr1 = imarr2
-#	time.sleep(1)
-	
+	data = np.fromstring(image_data.getvalue(), dtype=np.uint8)
+	imarr1 = cv2.imdecode(data, 1)
+	image_data.reset()
+
+	while 1:
+		image_data = StringIO.StringIO()
+		camera.capture(image_data, format="bmp", use_video_port=True)
+		data = np.fromstring(image_data.getvalue(), dtype=np.uint8)
+		imarr2 = cv2.imdecode(data, 1)
+		print float(((imarr2-imarr1)**2).sum())/(imarr1**2).sum()
+		imarr1 = imarr2
+	#	time.sleep(1)
+		
